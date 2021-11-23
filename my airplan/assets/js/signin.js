b4a3be5f912c71cee41f699999178569b9ade7ec
Miuno Raven <r0797856@student.thomasmore.be>
2021-11-23 23:04:39 +0100

    login and register

---
+++ my airplan/assets/js/signin.js
@@ -0,0 +1,4 @@
+var buttonElement = document.querySelector(".button-signin");
+var inputUsernameElement = document.querySelector("#input-email");
+var inputPasswordElement = document.querySelector("#input-pass");
+
