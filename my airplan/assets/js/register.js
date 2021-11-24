bf1280fe05480132c485f9889834c2ba7a20ad06
Miuno Raven <miunoraven@student.kuleuven.be>
2021-11-24 12:47:06 +0100

    fixing register

--- my airplan/assets/js/register.js
+++ my airplan/assets/js/register.js
@@ -9,11 +9,11 @@ var errorMailElement = document.querySelector(".error_mail");
 var errorPassElement = document.querySelector(".error_pass");
 
 // hardcoded user
-var user = { 
+
+var users = [{ 
     email: "r0797856@student.thomasmore.be",
     password: "hello" 
-};
-
+}];
 
 // when trying to register
 buttonElement.addEventListener("click", function(){
@@ -21,31 +21,49 @@ buttonElement.addEventListener("click", function(){
     var given_confirm_mail = inputMailConfirmElement.value;
     var given_pass = inputPasswordElement.value;
     var given_confirm_pass = intputPassConfirmElement.value;
-    
-    //if the email is already in the database
-    if(given_mail == user.email){
-        errorMessage(errorMailElement, "There is already an account on this e-mail. Try another e-mail");
-    }
+    var authenticated_mail = false;
+    var authenticated_pass = false;
 
+
+    console.log(users.forEach(user=>user.email));
+    //if the email is already in the database
+    if(users.find(user => user.email == given_mail)){
+        errorMessage(errorMailElement, "There is already an account on this e-mail. Try another e-mail");
+        authenticated_mail = false;
+    }
     //if the confirmation is wrong
     else if(given_mail !== given_confirm_mail){
         errorMessage(errorMailElement, "Your e-mail addresses are not the same. Confirm again.");  
+        authenticated_mail = false;
     }
 
     //delete the error messages if nothing is wrong
     else{
         errorMailElement.innerHTML="";
+        authenticated_mail = true;
     }
 
     //idem
     if(given_pass != given_confirm_pass){
-        errorMessage(errorPassElement, "Your passwords are not the same. Confirm again.")
+        errorMessage(errorPassElement, "Your passwords are not the same. Confirm again.");
+        authenticated_pass = false;
     }
 
     else{
         errorPassElement.innerHTML="";
+        authenticated_pass = true;
     }
 
+    if(authenticated_mail && authenticated_pass){
+        var user = { 
+            email: given_mail,
+            password: given_pass 
+        }; 
+        users.push(user);
+        buttonLinkElement.href="search_flight_numb.html";
+    }
+
+    
 
 
 });
