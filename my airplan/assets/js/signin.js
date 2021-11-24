d78a8d90618cc6b1f7f81b350bfadd23ff114acf
Miuno Raven <miunoraven@student.kuleuven.be>
2021-11-24 09:16:13 +0100

    added working login

--- my airplan/assets/js/signin.js
+++ my airplan/assets/js/signin.js
@@ -1,4 +1,22 @@
 var buttonElement = document.querySelector(".button-signin");
-var inputUsernameElement = document.querySelector("#input-email");
+var inputUsermailElement = document.querySelector("#input-email");
 var inputPasswordElement = document.querySelector("#input-pass");
+var linkHtmlElement = document.querySelector(".link_to_html");
 
+
+var user = { 
+    usermail: "r0797856@student.thomasmore.be",
+    password: "hello" 
+};
+
+buttonElement.addEventListener("click", function(){
+    var given_email = inputUsermailElement.value;
+    var given_pass = inputPasswordElement.value;
+
+    if(given_email == user.usermail && given_pass == user.password){
+        linkHtmlElement.href="search_flight_numb.html";
+    }
+    else{
+        alert("lol nope");
+    }
+})
