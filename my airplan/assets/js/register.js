a9702dfaba66ba891a391232c96440fd71e7bd73
Miuno Raven <miunoraven@student.kuleuven.be>
2021-11-24 11:53:23 +0100

    adding error messages

--- my airplan/assets/js/register.js
+++ my airplan/assets/js/register.js
@@ -1,32 +1,61 @@
 var inputMailElement = document.querySelector("#input-email");
-var inputMailConfirmElement = document.querySelector("#input-conf-mail");
+var inputMailConfirmElement = document.querySelector("#input-conf-email");
 var inputPasswordElement = document.querySelector("#input-pass");
 var intputPassConfirmElement = document.querySelector("#input-conf-pass");
 var buttonElement = document.querySelector(".button-register");
 var buttonLinkElement = document.querySelector(".link_button");
 
 var errorMailElement = document.querySelector(".error_mail");
+var errorPassElement = document.querySelector(".error_pass");
 
-
+// hardcoded user
 var user = { 
     email: "r0797856@student.thomasmore.be",
     password: "hello" 
 };
 
+
+// when trying to register
 buttonElement.addEventListener("click", function(){
     var given_mail = inputMailElement.value;
+    var given_confirm_mail = inputMailConfirmElement.value;
     var given_pass = inputPasswordElement.value;
+    var given_confirm_pass = intputPassConfirmElement.value;
+    
+    //if the email is already in the database
     if(given_mail == user.email){
-        alert("nice alvast in de if");
-        var errorMailPElement = document.querySelector(".error_mail p");
-        if(errorMailPElement == null){
-            alert("yoooooooooo");
-            var errorElement = document.createElement("p");
-            errorElement.innerHTML= "There is already an account on this e-mail. Try another e-mail"
-            errorMailElement.append(errorElement);
-        }
+        errorMessage(errorMailElement, "There is already an account on this e-mail. Try another e-mail");
     }
-    alert("nope not happening");
+
+    //if the confirmation is wrong
+    else if(given_mail !== given_confirm_mail){
+        errorMessage(errorMailElement, "Your e-mail addresses are not the same. Confirm again.");  
+    }
+
+    //delete the error messages if nothing is wrong
+    else{
+        errorMailElement.innerHTML="";
+    }
+
+    //idem
+    if(given_pass != given_confirm_pass){
+        errorMessage(errorPassElement, "Your passwords are not the same. Confirm again.")
+    }
+
+    else{
+        errorPassElement.innerHTML="";
+    }
+
+
+
 });
+
+//function that displays the error message
+function errorMessage(div, message){
+    var errorElement = document.createElement("p");
+    div.innerHTML="";
+    errorElement.innerHTML= message;
+    div.append(errorElement);
+}
 
 
