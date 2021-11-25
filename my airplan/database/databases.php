fbfbf1cc66fef59dc94e80e37bad6f5c1a7e89d9
Miuno Raven <miunoraven@student.kuleuven.be>
2021-11-25 13:26:39 +0100

    Update databases.php

--- my airplan/database/databases.php
+++ my airplan/database/databases.php
@@ -1,22 +1,34 @@
-<?php
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "root";
-    $dbName = "airplan";
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Databases</title>
+    </head>
+    <body>
+        <?php
+            $dbServername = "localhost";
+            $dbUsername = "root";
+            $dbPassword = "root";
+            $dbName = "airplan";
 
-    	
-    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-    
-    if ($conn == false) {
-        echo "Connection failed"; 
-    }
+                
+            $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+            
+            if ($conn == false) {
+                echo "Connection failed"; 
+            }
 
-    $sql = "SELECT * FROM airports;";
-    $result = mysqli_query($conn, $sql);
+            $sql = "SELECT * FROM airports;";
+            $result = mysqli_query($conn, $sql);
 
-    	
-    foreach ($airport as $result->fetch_all(MYSQLI_ASSOC)) {
-        echo $airport["name"];
-        echo $airport["country"];
-    }
-?>
+                
+            foreach ($airport as $result->fetch_all(MYSQLI_ASSOC)) {
+                echo $airport["name"];
+                echo $airport["country"];
+            }
+        ?>
+        
+    </body>
+</html>
