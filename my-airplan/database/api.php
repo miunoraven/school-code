8faeac9cb9f2d09cd2cb02a6223677e9a3de2f58
Miuno Raven <r0797856@student.thomasmore.be>
2021-11-30 21:04:54 +0100

    Update api.php

--- my-airplan/database/api.php
+++ my-airplan/database/api.php
@@ -1,29 +1,31 @@
 <?php
 
-$request = new HttpRequest();
-$request->setUrl('https://aerodatabox.p.rapidapi.com/flights/airports/icao/EBBR/2021-10-04T20:00/2021-10-05T08:00');
-$request->setMethod(HTTP_METH_GET);
+$curl = curl_init();
 
-$request->setQueryData([
-	'withLeg' => 'true',
-	'withCancelled' => 'true',
-	'withCodeshared' => 'true',
-	'withCargo' => 'true',
-	'withPrivate' => 'true',
-	'withLocation' => 'false'
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://aerodatabox.p.rapidapi.com/flights/airports/icao/EBBR/2021-10-04T20:00/2021-10-05T08:00?withLeg=true&withCancelled=true&withCodeshared=true&withCargo=true&withPrivate=true&withLocation=false",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"x-rapidapi-host: aerodatabox.p.rapidapi.com",
+		"x-rapidapi-key: 27dfaea46cmsh079a1a851443691p1bb619jsn2e2776ada877"
+	],
 ]);
 
-$request->setHeaders([
-	'x-rapidapi-host' => 'aerodatabox.p.rapidapi.com',
-	'x-rapidapi-key' => '27dfaea46cmsh079a1a851443691p1bb619jsn2e2776ada877'
-]);
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-try {
-	$response = $request->send();
+curl_close($curl);
 
-	echo $response->getBody();
-} catch (HttpException $ex) {
-	echo $ex;
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
 }
 
 ?>
