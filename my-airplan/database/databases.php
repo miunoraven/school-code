8651930a0641b2aed3972aa5cb7ea9e7e464f247
Miuno Raven <r0797856@student.thomasmore.be>
2021-12-07 21:42:25 +0100

    Update databases.php

--- my-airplan/database/databases.php
+++ my-airplan/database/databases.php
@@ -1,35 +1,122 @@
 <?php
     class Database {
-        public $dbServername = "localhost";
-        public $dbUsername = "root";
-        public $dbPassword = "root";
-        public $dbName = "airplan"; 
+        private $dbServername = "localhost";
+        private $dbUsername = "root";
+        private $dbPassword = "root";
+        private $dbName = "airplan"; 
+        // private $conn;
 
-        function getQuery($sql) {
+        // public $arrayUsers;
 
-        }  
+        public function __construc(){
+            mysql_connect($this->dbServername, $this->dbUsername, $this->dbPassword);  
+            mysql_select_db($this->db); 
+            
+        }
+        private function query_executed($sql)  
+        {  
+            $conn = new mysqli($this->dbServername, $this->dbUsername, $this->dbPassword,$this->db);
+            $result = mysqli_query($conn, $sql);
+            return $result;  
+        }
+
+        public function get_rows($tablename)  
+        {  
+            $sql = "SELECT * FROM $tablename;";  
+            $results = $this->query_executed($sql);  
+            echo var_dump($result);
+            $rows = $result->fetch_all(MYSQLI_ASSOC);  
+            return $rows;  
+        } 
+
+        public function get_fetch_data($r)  
+        {  
+            $array = array();  
+            while ($rows = $r->fetch_all(MYSQLI_ASSOC))  
+            {  
+                $array[] = $rows;  
+            }  
+            return $array;  
+        } 
+
+
+        public function getAllData(){    
+            $airports = [];
+            $sql = "SELECT * FROM airports;";
+            $result = mysqli_query($this->conn, $sql);
+            echo var_dump($result);
+                
+            // foreach ($result->fetch_all(MYSQLI_ASSOC) as $airport) {
+            //     $input = array(
+            //         "name" => $airport["name"],
+            //         "country" => $airport["country"],
+            //         "iata" => $airport["iata"],
+            //         "icao" => $airport["icao"]
+            //     );
+            //     array_push($airports, $input);
+            // }
+            // return $airports;
+        
+        }
+
     }
 
+    $database = new Database();
+    $array = $database->get_rows("airports");
+    echo $array;
+    // 
+
     
-        
+
+    
+    // function addData(){
+    //     //connecting to database
+    //     $dbServername = "localhost";
+    //     $dbUsername = "root";
+    //     $dbPassword = "root";
+    //     $dbName = "harrypotter";
+    
+    //     $conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+    
+    //     if ($conn->connect_error) {
+    //         die("Connection failed: " . $conn->connect_error);
+    //     }
+    
+    //     //gets all values from form input
+    //     $fname = $_POST["firstname"];
+    //     $lname = $_POST["lastname"];
+    //     $age =  $_POST["age"];
+    //     $gender = $_POST["gender"];
+    //     $description = $_POST["description"];
+    //     $sql = "INSERT INTO wizard VALUES (\"{$fname}\", \"{$lname}\", {$age}, \"{$gender}\", \"{$description}\", 0, 0, 0, 0);";
+    
+    //     //display completionpage if succeeded
+    //     if ($conn->query($sql) === TRUE) {
+    //         echo "yey";
+
+    //     } else {
+    //         echo "Error: " . $sql . "<br>" . $conn->error;
+    //     }
+    //     $conn->close();
+    // } 
 
             
-        $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    //     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
         
-        if ($conn == false) {
-            echo "Connection failed";
-        } 
+    //     if ($conn == false) {
+    //         echo "Connection failed";
+    //     } 
 
-        $sql = "SELECT * FROM airports;";
-        $result = mysqli_query($conn, $sql);
+    //     $sql = "SELECT * FROM airports;";
+    //     $result = mysqli_query($conn, $sql);
 
             
-        foreach ($result->fetch_all(MYSQLI_ASSOC) as $airport) {
-            echo $airport["name"];
-            echo $airport["country"];
-        } 
+    //     foreach ($result->fetch_all(MYSQLI_ASSOC) as $airport) {
+    //         echo $airport["name"];
+    //         echo $airport["country"];
+    //     } 
 
         
             
-        $conn->close();
+    //     $conn->close();
 
