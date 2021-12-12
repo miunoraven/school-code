c9f89328af413e51d05616070f121308fdf6d7c7
Miuno Raven <r0797856@student.thomasmore.be>
2021-12-12 23:01:45 +0100

    h

--- my-airplan/register.php
+++ my-airplan/register.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Register</title>
-        <?php include "fonts.php"; ?>
-        <link rel="stylesheet" href="assets/css/register.css" type="text/css">
+        <?php include "/components/fonts.php"; ?>
+        <!-- <link rel="stylesheet" href="assets/css/register.css" type="text/css"> -->
     </head>
     <body>
         <div class="background">
@@ -16,22 +16,30 @@
                 <div class="register">
                     <h2>Make a new account</h2>
                     <form action="" method="post">
-
-                        <input type="email" id="input-email" placeholder="E-mail" required>
+                        <input type="text" name="firstname" id="firstname" placeholder="Firstname" required>
                         <br> <br>
-                        <input type="email" id="input-conf-email" placeholder="Confirm e-mail" required>
+                        <input type="text" name="lastname" id="lastname" placeholder="Lastname" required>
+                        <br> <br>
+                        <input type="email" id="input-email" name="email" placeholder="E-mail" required>
+                        <br> <br>
+                        <input type="email" id="input-conf-email" name="conf_email" placeholder="Confirm e-mail" required>
                         <div class="error_mail"></div>
                         <br>                  
-                        <input type="password" id="input-pass" placeholder="Password" required>
+                        <input type="password" id="input-pass" name="pass" placeholder="Password" required>
                         <br> <br>
-                        <input type="password" id="input-conf-pass" placeholder="Confirm password" required>
+                        <input type="password" id="input-conf-pass" name="conf_pass" placeholder="Confirm password" required>
                         <div class="error_pass"></div>
+                        <br>
                         <input type="submit" value="signin" class="button-register">
                     </form>
+                    <?php
+                        $fname = $_POST("firstname");
+                        echo $fname; ?>
+
                     <p>Already have an account? <a href="index.php" class="signin">Sign in</a></p>
                 </div>
             </div>
         </div>
-        <script src="assets/js/register.js"></script>
+        <!-- <script src="assets/js/register.js"></script> -->
     </body>
 </html>
