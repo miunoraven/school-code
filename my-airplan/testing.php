0235da1ae543c7b244bf9ee135b1a46a37cc7f7e
Miuno Raven <r0797856@student.thomasmore.be>
2021-12-13 20:38:53 +0100

    Update testing.php

--- my-airplan/testing.php
+++ my-airplan/testing.php
@@ -2,4 +2,5 @@
     include "database/api.php";
     $api = new API("2021-10-31T06:00","2021-10-31T18:00");
     $array = $api->$response;
+    echo "aaaaaaaaaaaaaaaaaaaaaaa";
     var_dump($array);
