82823dd22f64da1d157828d93cf0b8bef399397a
Miuno Raven <r0797856@student.thomasmore.be>
2021-12-23 17:59:22 +0100

    parking

--- my-airplan/components/footer.php
+++ my-airplan/components/footer.php
@@ -1,4 +1,4 @@
 <footer>
-  <p class="footer_p">Author: Miuno Raven</p>
+  <p class="footer_p" id="footer_name">Author: Miuno Raven</p>
   <p class="footer_p">Contact: <a href="mailto:hege@example.com">r0797856@student.thomasmore.be</a></p>
 </footer>
--- my-airplan/parking.php
+++ my-airplan/parking.php
@@ -12,41 +12,54 @@
         <?php include "components/navigation.php"; ?>
         <div class="content">
             <h1>Parkings</h1>
-            <p class="text">You can find more than 10,000 parking spaces just a 5-minute walk from the terminal. Another 2,000 parking spaces at just a 10-minute walk from the airport terminal. Or would you rather jump on the free shuttle bus that takes you directly to the airport? Want a guaranteed parking space? Reserve your space in advance.</p>
-            <img src="assets/images/parking.png" alt="parking" id="parking_img">
-            <div class="table">
-                <table class="parking_table">
-                    <tr>
-                        <th>Parking</th>
-                        <th>Price 1 hour</th>
-                        <th>Price 1 day</th>
-                        <th>Price 1 week</th>
-                        <th>Walking distance</th>
-                        <th>Recommended stay</th>
-                        <th>Can use PCard+</th>
-                        <th>Online reservation</th>
-                    </tr>
-                    <?php
-                        include "database/databases.php";
-                        $database = new Database();
-                        $parkings = $database->getData("`parkings`");
-                        for($i=0;$i<sizeof($parkings);$i++){
-                            echo "<tr>";
-                            echo "<td>". $parkings[$i]["name"] . "</td>";
-                            echo "<td>". $parkings[$i]["price_1h"] . "€</td>";
-                            echo "<td>". $parkings[$i]["price_24h"] . "€</td>";
-                            echo "<td>". $parkings[$i]["price_1w"] . "€</td>";
-                            echo "<td>". $parkings[$i]["distance"] . " min</td>";
-                            echo "<td>". $parkings[$i]["rec_days"] . " days</td>";
-                            if($parkings[$i]["pcard"] == 1) echo "<td>Yes</td>";
-                            else echo "<td>No</td>";
-                            if($parkings[$i]["online_res"] == 1) echo "<td>Yes</td>";
-                            else echo "<td>No</td>";
-                            echo "</tr>";
-                        }
-                    ?>
-                </table>
+            <div class="pair">
+                <div class="information">
+                    <h3>Interparking</h3>
+                    <p class="text">You can find more than 10,000 parking spaces just a 5-minute walk from the terminal. Another 2,000 parking spaces at just a 10-minute walk from the airport terminal. Or would you rather jump on the free shuttle bus that takes you directly to the airport? Want a guaranteed parking space? Reserve your space in advance.</p>
+                    <h3>Make a reservation</h3>
+                    <a href="https://brusselsairport.interparking.be/nl/">Interparking Website</a>
+                    <p class="text">Tel: <a href="tel:+3227152110">00 32 (0)2 715 21 10</a></p>
+                    <p class="text">E-mail: <a href="mailto:brusselsairport@interparking.com">brusselsairport@interparking.com</a></p>
+                </div>
+                <img src="assets/images/interparking.jpg" alt="parking" id="interparking">
             </div>
+            <h3>Overview of all the parkings</h3>
+            <div class="information">
+                <div class="table">
+                    <table class="parking_table">
+                        <tr>
+                            <th>Parking</th>
+                            <th>Price 1 hour</th>
+                            <th>Price 1 day</th>
+                            <th>Price 1 week</th>
+                            <th>Walking distance</th>
+                            <th>Recommended stay</th>
+                            <th>Can use PCard+</th>
+                            <th>Online reservation</th>
+                        </tr>
+                        <?php
+                    include "database/databases.php";
+                    $database = new Database();
+                    $parkings = $database->getData("`parkings`");
+                    for($i=0;$i<sizeof($parkings);$i++){
+                        echo "<tr>";
+                        echo "<td>". $parkings[$i]["name"] . "</td>";
+                        echo "<td>". $parkings[$i]["price_1h"] . "€</td>";
+                        echo "<td>". $parkings[$i]["price_24h"] . "€</td>";
+                        echo "<td>". $parkings[$i]["price_1w"] . "€</td>";
+                        echo "<td>". $parkings[$i]["distance"] . " min</td>";
+                        echo "<td>". $parkings[$i]["rec_days"] . " days</td>";
+                        if($parkings[$i]["pcard"] == 1) echo "<td>Yes</td>";
+                        else echo "<td>No</td>";
+                        if($parkings[$i]["online_res"] == 1) echo "<td>Yes</td>";
+                        else echo "<td>No</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                    </table>
+                </div>
+            </div>
+            <img src="assets/images/parking.png" alt="parking" id="parking_img" width="700px">
         </div>
         <?php include "components/footer.php";?>
     </body>
