8bd13d4c3b463fe80e3b226dacdf621808455b56
Miuno Raven <r0797856@student.thomasmore.be>
2021-12-28 15:20:04 +0100

    css main

--- my-airplan/main.php
+++ my-airplan/main.php
@@ -36,53 +36,52 @@
             <div class="container">
                 <?php include "components/navigation.php"; ?>
                 <div class="content">
-                <h1>My flights</h1>
-                <?php
-                    for($i=0;$i<sizeof($flights);$i++){
-                        $flight = $flights[$i];
-        
-                        $output = $flight["time_dep"];
-                        list($date, $time) = explode(" ", $output);
-                        list($dep, $delay) = explode("+", $time);
-        
-                        $airport = $database->getAirport($flight["airport_dep"]);                
-        
-                        ?>
-                        <div class="ticket">
+                    <div class="tickets_div">
+                        <h1>My flights</h1>
+                        <?php
+                        for($i=0;$i<sizeof($flights);$i++){
+                            $flight = $flights[$i];
+            
+                            $output = $flight["time_dep"];
+                            list($date, $time) = explode(" ", $output);
+                            list($dep, $delay) = explode("+", $time);
+            
+                            $airport = $database->getAirport($flight["airport_dep"]);                
+            
+                            ?>
+                            <div class="ticket">
                                 <h2 id="boardingpass">Boarding Pass</h2>
                                 <div id="route">
-                                    <p class="rout_text" id="dest"><span style="font-size: 10pt;">From </span><br><?php echo $airport[0]["name"]; ?></p>
-                                    <p class="rout_text" id="arr"><span style="font-size: 10pt;">To </span> <br><?php echo $flight["airport_arr"]; ?></p>
-                                    <p class="rout_text" id="time"><span style="font-size: 10pt;">Departure on </span><br><?php echo $dep; ?></p>
+                                    <p class="rout_text" id="dest"><span class="title_info">From </span><br><?php echo $airport[0]["name"]; ?></p>
+                                    <p class="rout_text" id="arr"><span class="title_info">To </span> <br><?php echo $flight["airport_arr"]; ?></p>
+                                    <p class="rout_text" id="time"><span class="title_info">Departure on </span><br><?php echo $dep; ?></p>
                                 </div>
                                 <div id="info">
-                                    <p class="result" id="gate">Gate <br><?php echo $flight["gate"]; ?></p>
-                                    <p class="result" id="checkin">Check-in <br><?php echo $flight["check_in"]; ?></p>
-                                    <p class="result" id="status">Status<br><?php echo $flight["status"]; ?></p>
-                                    <p class="result" id="airline">Airline<br><?php echo $flight["airline"]; ?></p>
+                                    <p class="result" id="gate"><span class="title_info">Gate</span> <br><?php echo $flight["gate"]; ?></p>
+                                    <p class="result" id="checkin"><span class="title_info">Check-in</span> <br><?php echo $flight["check_in"]; ?></p>
+                                    <p class="result" id="status"><span class="title_info">Status</span><br><?php echo $flight["status"]; ?></p>
+                                    <p class="result" id="airline"><span class="title_info">Airline</span><br><?php echo $flight["airline"]; ?></p>
                                 </div>
                             </div>
-                            <button type="button" class="collapsible">Open Collapsible</button>
+                            <button type="button" class="collapsible">More information</button>
                             <div class="collapse_div">
-                                <div class="home">
-                                    <h3>At Home</h3>
-                                    <div class="corona">
-                                        <h4>Corona measures</h4>
+                                <div class="corona collaps_info">
+                                    <div class="text_info">
+                                        <h3>Corona measures</h3>
                                         <p class="corona_info">Make sure that you check which <span style="font-weight:bold">COVID-19</span> safety rules are applied to the country that you are traveling to. Make sure you have prepared all needed documents on time (Personal Health Fiche, vaccination certificate, PCR test results...). <br>
                                         In case you want to register for a <span style="font-weight:bold">PCR test on the aiport</span> <a href="https://brusselsairport.ecocare.center/">register here</a>.
-                                    </p>
+                                        </p>
                                     </div>
+                                    <img src="assets/images/pcr_center.jpg" alt="pcr" class="pcr" height="200px">
                                 </div>
-                                <div class="transprot">
+                                <div class="transport collapse_info">
                                     <h3>Transport</h3>
-                                    <div class="parking">
-                                        <a href="parking.php" class="home_a">Parkings</a>
-                                    </div>
-                                    <div class="transport">
-                                        <a href="transport.php" class="home_a">Public transport</a>
+                                    <div class="transport_links">
+                                        <a href="parking.php?id=<?php echo $id; ?>" class="transport_a">Parkings</a>
+                                        <a href="transport.php?id=<?php echo $id; ?>" class="transport_a">Public transport</a>
                                     </div>
                                 </div>
-                                <div class="checkin">
+                                <div class="checkin collapse_info">
                                     <h3>Check in / Drop off</h3>
                                     <a href="map.php?id=<?php echo $id; ?>"><img src="assets/images/pin_map.png" alt="pin" id="pin" height="50px"></a>
                                     <p>Check in <br><?php echo $flight["check_in"]; ?></p>
@@ -97,16 +96,16 @@
                                         calcTime($dep, 40);
                                     ?> </p>
                                 </div>
-                                <div class="security">
+                                <div class="security collapse_info">
                                     <h3>Security</h3>
                                     <img src="assets/images/security.jpeg" alt="security" class="security_img" height="300px">
                                 </div>
-                                <div class="freetime">
+                                <div class="freetime collapse_info">
                                     <h3>Free time</h3>
                                     <img src="assets/images/dutyfree_a.jpg" alt="dutyfree_a" class="dutyfree_a" height="200px">
                                     <a href="map.php?id=<?php echo $id; ?>"><img src="assets/images/pin_map.png" alt="pin" id="pin" height="50px"></a>
                                 </div>
-                                <div class="gate">
+                                <div class="gate collapse_info">
                                     <h3>Boarding</h3>
                                     <p>Gate<br><?php echo $flight["gate"]; ?></p>
                                     <p>Start boarding<br><?php
@@ -118,8 +117,9 @@
                                 </div>
                             </div>
                         <?php
-                    }
-                    ?>
+                        }
+                        ?>
+                    </div>
                 </div>
                 <?php include "components/footer.php";?>
             </div>
